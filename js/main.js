b2be7422112975dd203387042200086caa135473
Joey <joeylau1998@gmail.com>
2018-02-09 09:40:08 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import cat from 'cat';
+import basket from 'basket';
+import bomb from 'bomb';
+import health from 'health';
+import music from 'music';
+import playButton from 'playButton';
+import restartButton from 'restartButton';
+import scoreboard from 'scoreboard';
+import timer from 'timer';
+
