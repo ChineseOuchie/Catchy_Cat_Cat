889efb393fc227cfb37d29cac7e63111ec2c8e06
Joey <joeylau1998@gmail.com>
2018-02-09 10:23:35 +0100

    imports

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// import cat from 'cat';
-// import basket from 'basket';
-// import bomb from 'bomb';
-// import * as health from './health.js';
-// import music from 'music';
-// import playButton from 'playButton';
-// import restartButton from 'restartButton';
-// import scoreboard from 'scoreboard';
-// import timer from 'timer';
+import * as cat from './cat.js';
+import * as basket from './basket.js';
+import * as bomb from './bomb.js';
+import * as health from './health.js';
+import * as music from './music.js';
+import * as playButton from './playButton.js';
+import * as restartButton from './restartButton.js';
+import * as scoreboard from './scoreboard.js';
+import * as timer from './timer.js';
 
 const game = document.getElementById("game");
 const context = game.getContext("2d");
