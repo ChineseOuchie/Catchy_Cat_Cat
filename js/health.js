2253b8c1b5f257ad210f81c096e1dd5abd676d1d
dionsantegoets <dionsantegoets1999@gmail.com>
2018-02-09 10:19:47 +0100

    alles

--- js/health.js
+++ js/health.js
@@ -1,3 +1,2 @@
-export function test(){
-    return console.log('test')
-};
+export {};
+
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // import cat from 'cat';
 // import basket from 'basket';
 // import bomb from 'bomb';
-import * as health from './health.js';
+// import * as health from './health.js';
 // import music from 'music';
 // import playButton from 'playButton';
 // import restartButton from 'restartButton';
 // import scoreboard from 'scoreboard';
 // import timer from 'timer';
 
-health.test()
+const game = document.getElementById("game");
+const context = game.getContext("2d");
+
