e1b3f13af59cfa9a43ec6bbebea81da283a0a103
Joey <joeylau1998@gmail.com>
2018-02-23 08:54:10 +0100

    timer

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@
 // import bomb from 'bomb';
 // import * as health from './health.js';
 // import music from 'music';
-// import playButton from 'playButton';
+import * as playButton from './playButton.js';
 // import restartButton from 'restartButton';
 // import scoreboard from 'scoreboard';
-// import timer from 'timer';
+import * as timer from './timer.js';
 
+document.getElementById("start_button").addEventListener("click", playButton.start);
+document.getElementById('start_button').addEventListener('click', timer);
 const game = document.getElementById("game");
 const context = game.getContext("2d");
 
--- js/timer.js
+++ js/timer.js
@@ -8,11 +8,50 @@
 //     clearInterval(x);
 // }
 
-const time = new Date(60).getTime();
-let timer = setInterval(function () {
-    let now = new Date().getTime();
-    let countdown = time - now;
-    let second = Math.floor((countdown % (1000 * 60)) / 1000);
-    console.log(second);
-},1000);
+// const time = new Date(60).getTime();
+// let timer = setInterval(function () {
+//     let now = new Date().getTime();
+//     let countdown = time - now;
+//     let second = Math.floor((countdown % (1000 * 60)) / 1000);
+//     console.log(second);
+// },1000);
 
+let countdown;
+//click to begin
+document.getElementById('start_button').addEventListener('click', function(){
+    let seconds = 59;
+    clearInterval(countdown);
+    const nu = Date.now();
+    const over = nu + seconds * 1000;
+    showTime(seconds);
+    
+    countdown = setInterval(
+        function(){
+        const secondsLeft = Math.round((over - Date.now()) / 1000);
+        if (secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+        showTime(secondsLeft);
+    },1000)
+    //display time
+    function showTime(secondsLeft){
+        //anti cheat
+        if(secondsLeft >= 60){
+            clearInterval(countdown)
+            document.getElementById('tijd').innerHTML = "Don't cheat the time";
+
+        }else{
+                document.getElementById('tijd').innerHTML = secondsLeft;
+            //add '0' when < 10
+            if(secondsLeft < 10){
+                document.getElementById('tijd').innerHTML = '0'+ secondsLeft;
+                console.log('b')
+            }
+            //game over
+            if(secondsLeft === 0){
+                document.getElementById('tijd').innerHTML = "Game Over";
+            }
+        }
+    }
+})
