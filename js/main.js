d8a334ae9ccfa971bd1ca00f49288f038c3e3fb7
Joey <joeylau1998@gmail.com>
2018-03-16 08:57:17 +0100

    Back to good

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// import * as cat from './cat.js';
+import * as basket from './basket.js';
+// import * as bomb from './bomb.js';
+// import * as health from './health.js';
+// import * as music from './music.js';
+import * as playButton from './playButton.js';
+// import * as restartButton from './restartButton.js';
+// import * as scoreboard from './scoreboard.js';
+import * as timer from './timer.js';
+
+
+document.getElementById("start_button").addEventListener("click", playButton.start);
+document.getElementById('start_button').addEventListener('click', timer);
+const game = document.getElementById("game");
+const context = game.getContext("2d");
+context.font = '20px personalFont';
+basket.basket();
---
+++ js/timer.js
@@ -0,0 +1,48 @@
+const game = document.getElementById("game");
+const context = game.getContext("2d");
+const x = 30;
+const y = 40;
+let countdown;
+//click to begin
+document.getElementById('start_button').addEventListener('click', function(){
+    let seconds = 10;
+    clearInterval(countdown);
+    const nu = Date.now();
+    const over = nu + seconds * 1000;
+    showTime(seconds);
+
+    countdown = setInterval(
+        function(){
+        const secondsLeft = Math.round((over - Date.now()) / 1000);
+        if (secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+        showTime(secondsLeft);
+    },1000);
+    //display time
+    function showTime(secondsLeft){
+        //anti cheat
+        if(secondsLeft >= 60){
+            clearInterval(countdown);
+            document.getElementById('tijd').innerHTML = "Don't cheat the time";
+
+        }else{
+            context.clearRect(0, 0, game.width, game.height/2);
+            context.fillText(secondsLeft, x, y);
+            //add '0' when < 10
+            if(secondsLeft < 10){
+                context.clearRect(0, 0, game.width, game.height/2);
+                context.fillText(`0 ${secondsLeft}`, x, y);
+                // document.getElementById('tijd').innerHTML = '0'+ secondsLeft;
+            }
+            //game over
+            if(secondsLeft === 0){
+                context.font = '30px personalFont';
+                context.clearRect(0, 0, game.width, game.height);
+                context.fillText('Game Over', 70, 50);
+                // document.getElementById('tijd').innerHTML = "Game Over";
+            }
+        }
+    }
+});
